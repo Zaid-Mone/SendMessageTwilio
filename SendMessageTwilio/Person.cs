5b3ab61180d9f1cbf41fd08b1467d8fe90d129ca
Zaid-Mone <zaidmone9@gmail.com>
2022-11-06 22:49:28 +0200

    Add project files.

---
+++ SendMessageTwilio/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace SendMessageTwilio
+{
+    public class Person
+    {
+        public static string bodyMsg = Guid.NewGuid().ToString("N").Substring(0, 5);
+        public Guid Id { get; set; }
+        public string  Name { get; set; }
+        public int Age { get; set; }
+        public double  Salary { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string  VervicationCode { get; set; }
+
+        public Person()
+        {
+            this.Id = Guid.NewGuid();
+            Console.WriteLine("Enter your Name : ");
+            this.Name = Console.ReadLine();
+            Console.WriteLine("Enter your Age : ");
+            this.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Salary : ");
+            this.Salary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your PhoneNumber : ");
+            this.PhoneNumber = Console.ReadLine();
+            this.CreatedAt = DateTime.Now;
+            //Console.Clear();
+        }
+        public void SendMessage()
+        {
+            TwilioClient.Init("sid", "token");
+
+            var message = MessageResource.Create(
+                body: bodyMsg,
+                from: new Twilio.Types.PhoneNumber("+15617824386"),
+                to: new Twilio.Types.PhoneNumber(this.PhoneNumber)
+            );
+            
+            //Console.WriteLine(message.Sid);
+
+        }
+    }
+}
---
+++ SendMessageTwilio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SendMessageTwilio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Console.WriteLine($" Id : {person.Id} \n" +
+                $" Name : {person.Name} \n" +
+                $" Age : {person.Age} \n" +
+                $" Salary : {person.Salary} \n" +
+                $" Phone Number : {person.PhoneNumber} \n" +
+                $" Created At : {person.CreatedAt}");
+
+            person.SendMessage();
+
+            Console.WriteLine("Enter the Code : ");
+            person.VervicationCode = Console.ReadLine();
+
+            if(person.VervicationCode == Person.bodyMsg)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Sorry verfication code is incorrect");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
